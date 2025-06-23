15be3ab30d88a9c348f76f90311cb017e2b0574e
Denise Case <denisecase@gmail.com>
2025-06-22 19:57:14 -0500

    update app basics

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,14 +1,18 @@
 const CACHE_NAME = "ci-reps-cache-v1";
+const BASE_PATH = "/app-reps/";
 const urlsToCache = [
-  "/app-reps/",
-  "/app-reps/index.html",
-  "/app-reps/script.js",
-  "/app-reps/style.css",
-  "/app-reps/manifest.json",
-  "/app-reps/components/ci-rep.js",
-  "/app-reps/components/ci-footer.js",
-  "/app-reps/components/ci-header.js",
-  "/app-reps/status.json"
+  `${BASE_PATH}`,
+  `${BASE_PATH}index.html`,
+  `${BASE_PATH}index.js`,
+  `${BASE_PATH}style.css`,
+  `${BASE_PATH}manifest.json`,
+  `${BASE_PATH}components/ci-rep.js`,
+  `${BASE_PATH}status.json`,
+  "https://civic-interconnect.github.io/app-core/components/ci-header/ci-header.js",
+  "https://civic-interconnect.github.io/app-core/components/ci-footer/ci-footer.js",
+  "https://civic-interconnect.github.io/app-core/styles/tokens.css",
+  "https://civic-interconnect.github.io/app-core/styles/themes.css",
+  "https://civic-interconnect.github.io/app-core/styles/base.css",
 ];
 
 self.addEventListener("install", (event) => {
@@ -19,26 +23,31 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        )
       )
-    )
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-        return new Response("Offline and not cached", {
-          status: 503,
-          statusText: "Service Unavailable",
-          headers: { "Content-Type": "text/plain" }
-        });
-      });
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          return new Response("Offline and not cached", {
+            status: 503,
+            statusText: "Service Unavailable",
+            headers: { "Content-Type": "text/plain" },
+          });
+        })
+      );
     })
   );
 });
